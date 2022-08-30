3b7dcd2f24302337a89e1324fe63e7ad7dd98b97
Rumaro122 <yeshin0122@gmail.com>
2022-08-30 16:19:17 +0900

    add index.js feature promise related api

--- src/layers/routers/index.js
+++ src/layers/routers/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const authRouter = require("./auth.router");
+const promiseRouter = require("./promise.router");
 
 router.use("/auth", authRouter);
+router.use("/promise", promiseRouter);
 
 module.exports = router;
