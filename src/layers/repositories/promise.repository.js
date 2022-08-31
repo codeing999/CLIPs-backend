d8ea04f84c87232b69d13e7bd5a32597f3d60934
Rumaro122 <yeshin0122@gmail.com>
2022-08-31 12:22:25 +0900

    added feature to promise related api

--- src/layers/repositories/promise.repository.js
+++ src/layers/repositories/promise.repository.js
@@ -3,6 +3,50 @@ const sequelize = require("sequelize");
 
 class PromiseRepository {
 
+    createPromise = async (title, date, location, friendlist, penalty) => {
+        try{
+            await User.create({
+                title,
+                date,
+                location,
+                friendlist,
+                penalty
+            });
+        } catch (err) {
+            const error = new Error("FAILD_SQL");
+            error.code = 405;
+            throw error
+        };
+    };
+
+    getAllPromise = async () => {
+        try {
+            const response = await Promise.findAll({
+                order: [['date', 'DESC']],
+                attributes: { exclude: ['penalty','done']}
+            });
+            
+            return response;
+        } catch (err) {
+            const error = new Error("FAILD_SQL");
+            error.code = 405;
+            throw error
+        }
+    };
+
+    findFriend = async (phone) => {
+        try {
+            const response = await User.findOne({
+                attributes: ['phone', 'name'],
+                where : {phone: phone},
+            });
+            return response;
+        } catch (err) {
+            const error = new Error("FAILD_SQL");
+            error.code = 405;
+            throw error
+        }
+    };
 
 };
 
