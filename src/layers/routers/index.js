0b0cbcf9d5a438333c37dfd6f9e2837d5de5c851
Minsun91 <minsunkim167@gmail.com>
2022-08-31 05:20:55 +0200

    create feature:카카오로컬에서 정보 가져오기

--- src/layers/routers/index.js
+++ src/layers/routers/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-// const mainRouter = require("./main.router");
-
+const mainRouter = require("./main.router");
 //const authRouter = require("./auth.router");
+
+router.use("/main", mainRouter);
 
 module.exports = router;
