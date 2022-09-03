593a34ebf3b8925ffc22f363b8dc9a968ffa7fd3
jc <codeing999@gmail.com>
2022-09-03 14:50:13 +0900

    각 변수 길이 제한 수정 및 폰번호에 정규표현식 추가

--- src/joi_storage.js
+++ src/joi_storage.js
@@ -6,19 +6,13 @@ module.exports = class Validation {
       /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
     return joi
       .object({
-        email: joi
-          .string()
-          .min(5)
-          .max(50)
-          .required()
-          .regex(emailRegExp)
-          .messages({
-            "string.base": "Email은 문자열이어야 합니다.",
-            "any.required": "Email을 입력해주세요.",
-            "string.pattern.base": "Email이 형식에 맞지 않습니다.",
-            "string.min": "Email은 최소 5글자여야 합니다.",
-            "string.max": "Email은 최대 50글자여야 합니다.",
-          }),
+        email: joi.string().max(30).required().regex(emailRegExp).messages({
+          "string.base": "Email은 문자열이어야 합니다.",
+          "any.required": "Email을 입력해주세요.",
+          "string.pattern.base": "Email이 형식에 맞지 않습니다.",
+          //"string.min": "Email은 최소 5글자여야 합니다.",
+          "string.max": "Email은 최대 30글자여야 합니다.",
+        }),
       })
       .validateAsync({ email });
   };
@@ -26,11 +20,11 @@ module.exports = class Validation {
   validateNickname = (nickname) => {
     return joi
       .object({
-        nickname: joi.string().min(1).max(20).required().messages({
+        nickname: joi.string().max(8).required().messages({
           "string.base": "닉네임은 문자열이어야 합니다.",
           "any.required": "닉네임을 입력해주세요.",
-          "string.min": "닉네임은 최소 1글자여야 합니다.",
-          "string.max": "닉네임은 최대 20글자여야 합니다.",
+          //"string.min": "닉네임은 최소 1글자여야 합니다.",
+          "string.max": "닉네임은 최대 8글자여야 합니다.",
         }),
       })
       .validateAsync({ nickname });
@@ -39,11 +33,11 @@ module.exports = class Validation {
   validateName = (name) => {
     return joi
       .object({
-        name: joi.string().min(1).max(50).required().messages({
+        name: joi.string().max(20).required().messages({
           "string.base": "이름은 문자열이어야 합니다.",
           "any.required": "이름을 입력해주세요.",
-          "string.min": "이름은 최소 1글자여야 합니다.",
-          "string.max": "이름은 최대 50글자여야 합니다.",
+          //"string.min": "이름은 최소 1글자여야 합니다.",
+          "string.max": "이름은 최대 20글자여야 합니다.",
         }),
       })
       .validateAsync({ name });
@@ -57,7 +51,7 @@ module.exports = class Validation {
         password: joi
           .string()
           .min(8)
-          .max(30)
+          .max(16)
           .required()
           .regex(passwordRegExp)
           .messages({
@@ -65,7 +59,7 @@ module.exports = class Validation {
             "any.required": "비밀번호를 입력해주세요.",
             "string.pattern.base": "비밀번호가 형식에 맞지 않습니다.",
             "string.min": "비밀번호는 최소 8글자여야 합니다.",
-            "string.max": "비밀번호는 최대 30글자여야 합니다.",
+            "string.max": "비밀번호는 최대 16글자여야 합니다.",
           }),
       })
       .validateAsync({ password });
@@ -79,7 +73,7 @@ module.exports = class Validation {
         confirm: joi
           .string()
           .min(8)
-          .max(30)
+          .max(16)
           .required()
           .regex(passwordRegExp)
           .messages({
@@ -87,21 +81,29 @@ module.exports = class Validation {
             "any.required": "비밀번호 확인을 입력해주세요.",
             "string.pattern.base": "비밀번호 확인이 형식에 맞지 않습니다.",
             "string.min": "비밀번호 확인은 최소 8글자여야 합니다.",
-            "string.max": "비밀번호 확인은 최대 30글자여야 합니다.",
+            "string.max": "비밀번호 확인은 최대 16글자여야 합니다.",
           }),
       })
       .validateAsync({ confirm });
   };
 
   validatePhone = (phone) => {
+    const phoneRegExp = /^01[0-1, 7][0-9]{7,8}$/; //-없이 입력받을 때
     return joi
       .object({
-        phone: joi.string().min(5).max(20).required().messages({
-          "string.base": "휴대폰 번호는 문자열이어야 합니다.",
-          "any.required": "휴대폰 번호를 입력해주세요.",
-          "string.min": "휴대폰 번호는 최소 5글자여야 합니다.",
-          "string.max": "휴대폰 번호는 최대 20글자여야 합니다.",
-        }),
+        phone: joi
+          .string()
+          .min(10)
+          .max(11)
+          .required()
+          .regex(phoneRegExp)
+          .messages({
+            "string.base": "휴대폰 번호는 문자열이어야 합니다.",
+            "any.required": "휴대폰 번호를 입력해주세요.",
+            "string.pattern.base": "휴대폰 번호가 형식에 맞지 않습니다.",
+            "string.min": "휴대폰 번호는 최소 10글자여야 합니다.",
+            "string.max": "휴대폰 번호는 최대 11글자여야 합니다.",
+          }),
       })
       .validateAsync({ phone });
   };
