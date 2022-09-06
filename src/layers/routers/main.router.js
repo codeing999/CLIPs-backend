2dcc18517936475889a4261c12b93f86fbf3b1d4
Minsun91 <minsunkim167@gmail.com>
2022-09-06 11:37:25 +0200

    develop 합친 후 커밋

--- src/layers/routers/main.router.js
+++ src/layers/routers/main.router.js
@@ -4,7 +4,6 @@ const MainController = require("../controllers/main.controller");
 const mainController = new MainController();
 
 // 메인페이지
-// router.post("/main", mainController.mainPage);
 router.get("/", mainController.mainPage);
 router.get("/crawl", mainController.imageUrl);
 
