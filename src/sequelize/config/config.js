af75114e7a660aaff90eb43fd3a38f9c10d81030
jc <codeing999@gmail.com>
2022-09-12 12:55:22 +0900

    env configure

---
+++ src/sequelize/config/config.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const env = process.env;
+
+const development = {
+  username: env.DB_PROD_USERNAME,
+  password: env.DB_PROD_PASSWORD,
+  database: env.DB_PROD_DATABASE,
+  host: env.DB_PROD_HOST,
+  dialect: env.DB_PROD_DIALECT,
+  //port: env.DB_PORT
+};
+
+const production = {
+  username: env.DB_PROD_USERNAME,
+  password: env.DB_PROD_PASSWORD,
+  database: env.DB_PROD_DATABASE,
+  host: env.DB_PROD_HOST,
+  dialect: env.DB_PROD_DIALECT,
+  //port: env.DB_PORT
+};
+
+const test = {
+  username: env.DB_USERNAME,
+  password: env.DB_PASSWORD,
+  database: env.DB_DATABASE_TEST,
+  host: env.DB_HOST,
+  dialect: env.DB_DIALECT,
+  //port: env.MYSQL_PORT
+};
+
+module.exports = { development, production, test };
